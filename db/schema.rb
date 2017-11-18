d1ecad4bdacb50a62b908ee3c5961b939742e2c8
mrjkc <julius.kvedaras@coupa.com>
2017-11-18 21:31:04 +0000

    the tweet resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118210525) do
+ActiveRecord::Schema.define(version: 20171118212547) do
+
+  create_table "tweets", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false

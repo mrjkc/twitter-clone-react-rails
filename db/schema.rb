291bcfc7010b25ca0f1e85c898b300d64e89509c
mrjkc <julius.kvedaras@coupa.com>
2017-11-19 11:21:24 +0000

    The Followers Resource

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171118212547) do
+ActiveRecord::Schema.define(version: 20171119105853) do
+
+  create_table "followers", force: :cascade do |t|
+    t.integer "user_id"
+    t.integer "followed_by"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "tweets", force: :cascade do |t|
     t.integer "user_id"
